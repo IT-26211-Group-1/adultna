ac741a16277e9689d9564cbeb00e93b70eecf2de
ledomnique <dominique.nilo20@gmail.com>
2025-08-28 23:52:42 +0800

    * Created first playwright test case - Login * Added role="alert" to FieldErrors in LoginForm

--- app/auth/login/_components/LoginForm.tsx
+++ app/auth/login/_components/LoginForm.tsx
@@ -72,7 +72,9 @@ export const LoginForm = () => {
             placeholder="Email"
             type="email"
           />
-          <p className="text-sm text-red-500 mt-1">{errors.email?.message}</p>
+          <p role="alert" className="text-sm text-red-500 mt-1">
+            {errors.email?.message}
+          </p>
         </div>
 
         {/* Password */}
@@ -83,7 +85,7 @@ export const LoginForm = () => {
             placeholder="Password"
             type="password"
           />
-          <p className="text-sm text-red-500 mt-1">
+          <p role="alert" className="text-sm text-red-500 mt-1">
             {errors.password?.message}
           </p>
         </div>
