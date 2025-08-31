eaa9e37d5b75f38edcc2309333879294e74c0799
Kurt <keduterte@gmail.com>
2025-09-01 01:29:13 +0800

    added footer

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,11 +1,13 @@
+import { Footer } from "@/components/ui/footer";
 import { Navbar } from "@/components/ui/Navbar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
+      {/* TODO: Fix Navbar and Footer  */}
       <Navbar />
       <main>{children}</main>
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 }
