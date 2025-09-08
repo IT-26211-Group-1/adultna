58adaffef155fc8e5f818b5658cdf49b845293d2
Kurt <keduterte@gmail.com>
2025-09-08 19:50:29 +0800

    fix lint and types issue

--- app/(public)/about/_components/AboutMembers.tsx
+++ app/(public)/about/_components/AboutMembers.tsx
@@ -1,41 +1,41 @@
 import { MemberCard } from "./MemberCard";
 
 export function AboutMembers() {
-    return (
-        <section className="w-full min-h-[500px] top-10 py-16 bg-white relative flex flex-col gap-8 px-4 md:px-22 max-w-6xl text-justify">
-            <h2 className="text-3xl md:text-4xl lg:text-5xl text-ultra-violet leading-tight font-playfair text-center ">
-                The <span className="text-crayola-orange">Team</span> Behind The Dream
-            </h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
-                <MemberCard 
-              name="Patricia Bianca Arellano"
-              description="Member Description"
-              linkedin="https://linkedin.com/"
-              github="https://github.com/"
-              bg="bg-olivine/50"
-            />
-            <MemberCard
-                name="Kurt Emmanuel Duterte"
-                description="Member Description"
-                linkedin="https://linkedin.com/"
-                github="https://github.com/"
-                bg="bg-periwinkle"
-            />
-            <MemberCard
-                name="Lewis Dominique Nilo"
-                description="Member Description"
-                linkedin="https://linkedin.com/"
-                github="https://github.com/"
-                bg="bg-olivine/50"
-            />
-            <MemberCard
-                name="Adrian Dale Relevo"
-                description="Member Description"
-                linkedin="https://linkedin.com/"
-                github="https://github.com/"
-                bg="bg-periwinkle"
-            />
-            </div>
-        </section>
-    );
+  return (
+    <section className="w-full min-h-[500px] top-10 py-16 bg-white relative flex flex-col gap-8 px-4 md:px-22 max-w-6xl text-justify">
+      <h2 className="text-3xl md:text-4xl lg:text-5xl text-ultra-violet leading-tight font-playfair text-center ">
+        The <span className="text-crayola-orange">Team</span> Behind The Dream
+      </h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+        <MemberCard
+          bg="bg-olivine/50"
+          description="Member Description"
+          github="https://github.com/"
+          linkedin="https://linkedin.com/"
+          name="Patricia Bianca Arellano"
+        />
+        <MemberCard
+          bg="bg-periwinkle"
+          description="Member Description"
+          github="https://github.com/"
+          linkedin="https://linkedin.com/"
+          name="Kurt Emmanuel Duterte"
+        />
+        <MemberCard
+          bg="bg-olivine/50"
+          description="Member Description"
+          github="https://github.com/"
+          linkedin="https://linkedin.com/"
+          name="Lewis Dominique Nilo"
+        />
+        <MemberCard
+          bg="bg-periwinkle"
+          description="Member Description"
+          github="https://github.com/"
+          linkedin="https://linkedin.com/"
+          name="Adrian Dale Relevo"
+        />
+      </div>
+    </section>
+  );
 }
---
+++ types/faq.ts
@@ -0,0 +1,7 @@
+export type MemberCardProps = {
+  name: string;
+  description: string;
+  linkedin: string;
+  github: string;
+  bg?: string;
+};
