032004fb9eb0f16ddd189c1159697675aef82f50
ledomnique <dominique.nilo20@gmail.com>
2025-09-15 13:47:54 +0800

    Created components for features page, missing assets (for now)

---
+++ app/(public)/features/_components/FeatureText.tsx
@@ -0,0 +1,12 @@
+export function FeatureText() {
+    return (
+        <section className="w-full min-h-[400px] top-10 py-16 bg-white relative flex flex-col gap-2 px-4 md:px-22 max-w-6xl mx-auto text-justify items-center">
+            <h2 className="mt-20 text-3xl md:text-4xl lg:text-5xl text-ultra-violet leading-tight font-playfair text-center">
+            Features
+            </h2>
+            <p className="text-lg md:text-xl text-gray-400 max-w-4xl mx-auto font-inter text-center">
+            Explore what AdultNa can offer you, for your personal growth and well-being.
+            </p>
+        </section>
+    );
+}
---
+++ app/(public)/features/_components/Filebox.tsx
@@ -0,0 +1,10 @@
+export function Filebox() {
+    return (
+        <section className="w-full min-h-[600px] top-10 py-16 bg-white relative flex flex-col gap-2 px-4 md:px-22 max-w-6xl text-justify">
+            <h2 className="text-2xl font-bold mb-4 drop-shadow-gray-700 text-ultra-violet">Adulting Filebox</h2>
+            <p className="text-base md:text-lg text-gray-400 max-w-lg font-inter">
+            The Adulting Filebox is a secure digital storage solution designed to help you organize and manage important documents related to your journey into adulthood. Whether it&#39;s personal identification, financial records, or educational certificates, the Filebox provides a centralized location to keep all your essential files easily accessible and well-organized.
+            </p>
+        </section>
+    );
+}
