9b3feb985144766705cee150ffffd3c43f893397
Kurt <keduterte@gmail.com>
2025-09-16 00:18:47 +0800

    seperated client into a smaller component

--- app/(public)/features/_components/Interview.tsx
+++ app/(public)/features/_components/Interview.tsx
@@ -1,10 +1,16 @@
 export function Interview() {
-    return (
-        <section className="w-full min-h-[600px] top-10 py-16 bg-white relative flex flex-col gap-2 px-4 md:px-22 max-w-6xl text-justify">
-            <h2 className="text-2xl font-bold mb-4 drop-shadow-gray-700 text-ultra-violet">Mock Interview Coach</h2>
-            <p className="text-base md:text-lg text-gray-400 max-w-lg font-inter">
-            Our Mock Interview Coach is an interactive tool designed to help you prepare for real-life job interviews. It simulates a variety of interview scenarios, providing you with practice questions and feedback to improve your responses, boost your confidence, and enhance your overall interview skills.
-            </p>
-        </section>
-    );
+  return (
+    <section className="w-full min-h-[600px] top-10 py-16 bg-white relative flex flex-col gap-2 px-4 md:px-22 max-w-6xl text-justify">
+      <h2 className="text-2xl font-bold mb-4 drop-shadow-gray-700 text-ultra-violet">
+        Mock Interview Coach
+      </h2>
+      <p className="text-base md:text-lg text-gray-400 max-w-lg font-inter">
+        Our Mock Interview Coach is an interactive tool designed to help you
+        prepare for real-life job interviews. It simulates a variety of
+        interview scenarios, providing you with practice questions and feedback
+        to improve your responses, boost your confidence, and enhance your
+        overall interview skills.
+      </p>
+    </section>
+  );
 }
