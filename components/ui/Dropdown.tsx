611751189f90a0fa329db1a6b75187a732bdfbd9
Adrian Relevo <adriandale.relevo.cics@ust.edu.ph>
2025-09-18 23:34:10 +0800

    Nothing inside yet

---
+++ components/ui/Dropdown.tsx
@@ -0,0 +1,15 @@
+import {Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Button} from "@heroui/react";
+
+export default function App() {
+  return (
+    <Dropdown>
+      <DropdownTrigger>
+        <Button variant="bordered">{}</Button>
+      </DropdownTrigger>
+      <DropdownMenu aria-label="Static Actions">
+
+        <DropdownItem key=""></DropdownItem>
+      </DropdownMenu>
+    </Dropdown>
+  );
+}
