6793ce9aa89d36011a81a5da5d231aff30ccb580
Adrian Relevo <adriandale.relevo.cics@ust.edu.ph>
2025-10-12 17:20:55 +0800

    ramoved searchparams while there is no backend to fix the lint warnings

--- app/resumes/editor/page.tsx
+++ app/resumes/editor/page.tsx
@@ -1,28 +1,10 @@
 import { Metadata } from "next";
 import ResumeEditor from "./_components/ResumeEditor";
-interface PageProps {
-  searchParams: Promise<{ resumeId?: string }>;
-}
 
 export const metadata: Metadata = {
   title: "Design your resume",
 };
 
-export default async function Page({ searchParams }: PageProps) {
-//   const { resumeId } = await searchParams;
-
-//   const { userId } = await auth();
-
-//   if (!userId) {
-//     return null;
-//   }
-
-//   const resumeToEdit = resumeId
-//     ? await prisma.resume.findUnique({
-//         where: { id: resumeId, userId },
-//         include: resumeDataInclude,
-//       })
-//     : null;
+export default async function Page() {
     return <ResumeEditor />;
-//   return <ResumeEditor resumeToEdit={resumeToEdit} />;
 }
