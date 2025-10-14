50be4aef3ea39eaf41223a281872d8284b36ff39
Adrian Relevo <adriandale.relevo.cics@ust.edu.ph>
2025-10-14 21:22:37 +0800

    Transferred the location of the filebox

---
+++ app/(protected)/filebox/page.tsx
@@ -0,0 +1,15 @@
+import { SearchSection } from "./_components/SearchSection";
+import { UploadDocument } from "./_components/UploadDocument";
+import { SecureDocument } from "./_components/SecureDocument";
+
+export default function FileBoxPage() {
+    return (
+        <div>
+            <h1>File Box</h1>
+            {/* <UploadDocument /> */}
+            <SearchSection />
+            {/* <SecureDocument /> */}
+            
+        </div>
+    );
+}
