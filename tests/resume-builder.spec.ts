cd138cb4971f65fe87596c9700f997370fbc45e5
ledomnique <dominique.nilo20@gmail.com>
2025-11-22 01:38:25 +0800

    added test cases in resume-builder test suite

---
+++ tests/resume-builder.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("Build A Resume", () => {
+    test("clicking add DOB button would reveal DOB field", async ({ page }) => {
+    });
+
+    test("clicking add LinkedIn button would reveal LinkedIn field", async ({ page }) => {
+    });
+
+    test("clicking add Portfolio/Website button would reveal Portfolio/Website field", async ({ page }) => {
+    });
+
+});
