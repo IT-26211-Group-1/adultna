d7c1730ca516d82953c8ec5577e15f86b3a2a904
Kurt <keduterte@gmail.com>
2025-11-26 16:49:44 +0800

    fix font blocking rendering and added s3 cache headers script

--- config/fonts.ts
+++ config/fonts.ts
@@ -1,4 +1,9 @@
-import { Fira_Code as FontMono, Inter as FontSans } from "next/font/google";
+import {
+  Fira_Code as FontMono,
+  Inter as FontSans,
+  Playfair_Display,
+  Song_Myung
+} from "next/font/google";
 
 export const fontSans = FontSans({
   subsets: ["latin"],
@@ -9,5 +14,19 @@ export const fontSans = FontSans({
 export const fontMono = FontMono({
   subsets: ["latin"],
   variable: "--font-mono",
+  display: "swap",
+});
+
+export const fontPlayfair = Playfair_Display({
+  subsets: ["latin"],
+  variable: "--font-playfair",
+  weight: ["400", "500", "600", "700"],
+  style: ["normal", "italic"],
+  display: "swap",
+});
+
+export const fontSongMyung = Song_Myung({
+  variable: "--font-songmyung",
+  weight: "400",
   display: "swap",
 });
