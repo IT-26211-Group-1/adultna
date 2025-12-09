7b7acbbfd7087efb7b381d22f0fb40712e53d0a2
ledomnique <dominique.nilo20@gmail.com>
2025-12-09 22:06:53 +0800

    * reworked tcs for the admin + verifier test suites

--- tests/admin-dashboard.spec.ts
+++ tests/admin-dashboard.spec.ts
@@ -14,7 +14,7 @@ test.describe("Admin dashboard", () => {
     test("admin login with invalid credentials shows error" , async ({ page }) => {
         //login with valid account first
         await page.goto("http://adultna.com/admin/login");
-        await page.fill('input[name="email"]', "lewisdomnilo@gmail.com");
+        await page.fill('input[name="email"]', "admin@gmail.com");
         await page.fill('input[name="password"]', "Lewis123.");
         await page.getByRole("button", { name: "Login" }).click();
 
@@ -22,16 +22,6 @@ test.describe("Admin dashboard", () => {
     });
 
     test("admin login with valid credentials redirects to admin dashboard" , async ({ page }) => {
-        //login with valid account first
-        await page.goto("http://adultna.com/admin/login");
-        await page.fill('input[name="email"]', "admin@gmail.com");
-        await page.fill('input[name="password"]', "QWEasd123.");
-        await page.getByRole("button", { name: "Login" }).click();
-
-        await expect(page).toHaveURL(/\/admin\/dashboard\/?$/, { timeout: 15000 });
-    });
-
-    test("admin logout redirects to login page" , async ({ page }) => {
         //login with valid account first
         await page.goto("http://adultna.com/admin/login");
         await page.fill('input[name="email"]', "admin@gmail.com");
@@ -49,16 +39,10 @@ test.describe("Admin dashboard", () => {
         await page.getByRole("button", { name: "Login" }).click();
 
         await expect(page).toHaveURL(/\/admin\/dashboard\/?$/, { timeout: 15000 });
-    });
 
-    test("user is able to be deactivated" , async ({ page }) => {
-        //login with valid account first
-        await page.goto("http://adultna.com/admin/login");
-        await page.fill('input[name="email"]', "admin@gmail.com");
-        await page.fill('input[name="password"]', "QWEasd123.");
-        await page.getByRole("button", { name: "Login" }).click();
+        await page.getByRole('link', { name: 'User Management' }).click();
 
-        await expect(page).toHaveURL(/\/admin\/dashboard\/?$/, { timeout: 15000 });
+        await expect(page).toHaveURL(/\/admin\/accounts\/?$/, { timeout: 15000 });
     });
 
     test("user feedback and reports page is accessible" , async ({ page }) => {
@@ -68,7 +52,9 @@ test.describe("Admin dashboard", () => {
         await page.fill('input[name="password"]', "QWEasd123.");
         await page.getByRole("button", { name: "Login" }).click();
 
-        await expect(page).toHaveURL(/\/admin\/dashboard\/?$/, { timeout: 15000 });
+        await page.getByRole('link', { name: 'User Feedback & Report' }).click();
+
+        await expect(page).toHaveURL(/\/admin\/feedback\/?$/, { timeout: 15000 });
     });
 
     test("add new feedback is successful" , async ({ page }) => {
@@ -78,17 +64,20 @@ test.describe("Admin dashboard", () => {
         await page.fill('input[name="password"]', "QWEasd123.");
         await page.getByRole("button", { name: "Login" }).click();
 
-        await expect(page).toHaveURL(/\/admin\/homepage\/?$/, { timeout: 15000 });
-    });
-
-    test("user feedback can be archived" , async ({ page }) => {
-        //login with valid account first
-        await page.goto("http://adultna.com/admin/login");
-        await page.fill('input[name="email"]', "admin@gmail.com");
-        await page.fill('input[name="password"]', "QWEasd123.");
-        await page.getByRole("button", { name: "Login" }).click();
-
         await expect(page).toHaveURL(/\/admin\/dashboard\/?$/, { timeout: 15000 });
+
+
+        await page.getByRole('link', { name: 'User Feedback & Report' }).click();
+        await page.getByRole('button', { name: 'Add Feedback' }).click();
+        await page.getByRole('textbox', { name: 'Title *' }).click();
+        await page.getByRole('textbox', { name: 'Title *' }).fill('Browser request permission isn\'t coming out');
+        await page.getByRole('textbox', { name: 'Description *' }).click();
+        await page.getByRole('textbox', { name: 'Description *' }).fill('When I\'m about to use the mock interview coach, and I wanna use the dictation feature, the pop up to ask for mic permission isn\'t coming out so I end up not using it.');
+        await page.getByLabel('Type *').selectOption('report');
+        await page.getByLabel('Related Feature *').selectOption('mock_interview_coach');
+        await page.getByRole('button', { name: 'Create Feedback' }).click();
+
+        await page.getByText('Feedback created successfully').isVisible();
     });
 
     test("content management - onboarding questions is accessible" , async ({ page }) => {
@@ -99,6 +88,10 @@ test.describe("Admin dashboard", () => {
         await page.getByRole("button", { name: "Login" }).click();
 
         await expect(page).toHaveURL(/\/admin\/dashboard\/?$/, { timeout: 15000 });
+
+        await page.getByRole('button', { name: 'Content Management' }).click();
+        await page.getByRole('link', { name: 'Onboarding questions' }).click();
+        await expect(page).toHaveURL(/\/admin\/content\/onboarding\/?$/, { timeout: 15000 });
     });
 
     test("content management - interview questions is accessible" , async ({ page }) => {
@@ -109,6 +102,10 @@ test.describe("Admin dashboard", () => {
         await page.getByRole("button", { name: "Login" }).click();
 
         await expect(page).toHaveURL(/\/admin\/dashboard\/?$/, { timeout: 15000 });
+
+        await page.getByRole('button', { name: 'Content Management' }).click();
+        await page.getByRole('link', { name: 'Interview questions bank' }).click();
+        await expect(page).toHaveURL(/\/admin\/content\/questions\/?$/, { timeout: 15000 });
     });
 
     test("content management - government guides page is accessible" , async ({ page }) => {
@@ -119,36 +116,10 @@ test.describe("Admin dashboard", () => {
         await page.getByRole("button", { name: "Login" }).click();
 
         await expect(page).toHaveURL(/\/admin\/dashboard\/?$/, { timeout: 15000 });
-    });
 
-    test("government guides can be archived" , async ({ page }) => {
-        //login with valid account first
-        await page.goto("http://adultna.com/admin/login");
-        await page.fill('input[name="email"]', "admin@gmail.com");
-        await page.fill('input[name="password"]', "QWEasd123.");
-        await page.getByRole("button", { name: "Login" }).click();
-
-        await expect(page).toHaveURL(/\/admin\/dashboard\/?$/, { timeout: 15000 });
-    });
-
-    test("interview questions can be archived" , async ({ page }) => {
-        //login with valid account first
-        await page.goto("http://adultna.com/admin/login");
-        await page.fill('input[name="email"]', "admin@gmail.com");
-        await page.fill('input[name="password"]', "QWEasd123.");
-        await page.getByRole("button", { name: "Login" }).click();
-
-        await expect(page).toHaveURL(/\/admin\/dashboard\/?$/, { timeout: 15000 });
-    });
-
-    test("onboarding questions can be archived" , async ({ page }) => {
-        //login with valid account first
-        await page.goto("http://adultna.com/admin/login");
-        await page.fill('input[name="email"]', "admin@gmail.com");
-        await page.fill('input[name="password"]', "QWEasd123.");
-        await page.getByRole("button", { name: "Login" }).click();
-
-        await expect(page).toHaveURL(/\/admin\/dashboard\/?$/, { timeout: 15000 });
+        await page.getByRole('button', { name: 'Content Management' }).click();
+        await page.getByRole('link', { name: 'Government processes' }).click();
+        await expect(page).toHaveURL(/\/admin\/content\/guides\/?$/, { timeout: 15000 });
     });
 
     test("government guides can be edited" , async ({ page }) => {
@@ -159,6 +130,14 @@ test.describe("Admin dashboard", () => {
         await page.getByRole("button", { name: "Login" }).click();
 
         await expect(page).toHaveURL(/\/admin\/dashboard\/?$/, { timeout: 15000 });
+
+        await page.getByRole('button', { name: 'Content Management' }).click();
+        await page.getByRole('link', { name: 'Government processes' }).click();
+        await page.getByRole('button').filter({ hasText: /^$/ }).nth(2).click();
+        await page.getByRole('menuitem', { name: 'Edit Guide' }).click();
+        await page.getByRole('textbox', { name: 'Guide Title *' }).fill('How to apply for A New Passport for New Applicants');
+        await page.getByRole('button', { name: 'Update Guide' }).click();
+        await page.getByText('Guide updated successfully').isVisible();
     });
 
     test("audit logs are accessible" , async ({ page }) => {
@@ -169,16 +148,9 @@ test.describe("Admin dashboard", () => {
         await page.getByRole("button", { name: "Login" }).click();
 
         await expect(page).toHaveURL(/\/admin\/dashboard\/?$/, { timeout: 15000 });
-    });
 
-    test("audit logs can be exported to csv file successfully" , async ({ page }) => {
-        //login with valid account first
-        await page.goto("http://adultna.com/admin/login");
-        await page.fill('input[name="email"]', "admin@gmail.com");
-        await page.fill('input[name="password"]', "QWEasd123.");
-        await page.getByRole("button", { name: "Login" }).click();
-
-        await expect(page).toHaveURL(/\/admin\/dashboard\/?$/, { timeout: 15000 });
+        await page.getByRole('link', { name: 'Audit Logs' }).click();
+        await expect(page).toHaveURL(/\/admin\/logs\/?$/, { timeout: 15000 });
     });
 
     test("audit logs can be sorted by the given metrics" , async ({ page }) => {
@@ -189,6 +161,13 @@ test.describe("Admin dashboard", () => {
         await page.getByRole("button", { name: "Login" }).click();
 
         await expect(page).toHaveURL(/\/admin\/dashboard\/?$/, { timeout: 15000 });
+
+        await page.getByRole('link', { name: 'Audit Logs' }).click();
+
+        await page.getByRole('button', { name: 'Last 30 Days' }).click();
+        await page.getByRole('button', { name: 'Last 90 Days' }).click();
+        await page.getByRole('button', { name: 'Today' }).click();
+        await page.getByRole('button', { name: 'Last 7 Days' }).click();
     });
     
 });
--- tests/verifier-dashboard.spec.ts
+++ tests/verifier-dashboard.spec.ts
@@ -3,61 +3,117 @@ import { test, expect } from "@playwright/test";
 test.describe("Verifier Dashboard functionalities", () => {
     test("is able to access verifier dashboard" , async ({ page }) => {
         //login with valid account first
-        await page.goto("http://adultna.com/auth/login");
-        await page.fill('input[name="email"]', "lewisdomnilo@gmail.com");
-        await page.fill('input[name="password"]', "Lewis123.");
+        await page.goto("http://adultna.com/admin/login");
+        await page.fill('input[name="email"]', "verifier@gmail.com");
+        await page.fill('input[name="password"]', "QWEasd123.");
         await page.getByRole("button", { name: "Login" }).click();
 
-        await expect(page).toHaveURL(/\/dashboard\/?$/, { timeout: 15000 });
+        await expect(page).toHaveURL(/\/admin\/dashboard\/?$/, { timeout: 15000 });
     });
 
     test("able to approve interview questions" , async ({ page }) => {
         //login with valid account first
-        await page.goto("http://adultna.com/auth/login");
-        await page.fill('input[name="email"]', "lewisdomnilo@gmail.com");
-        await page.fill('input[name="password"]', "Lewis123.");
+        await page.goto("http://adultna.com/admin/login");
+        await page.fill('input[name="email"]', "verifier@gmail.com");
+        await page.fill('input[name="password"]', "QWEasd123.");
         await page.getByRole("button", { name: "Login" }).click();
 
-        await expect(page).toHaveURL(/\/dashboard\/?$/, { timeout: 15000 });
+        await expect(page).toHaveURL(/\/admin\/dashboard\/?$/, { timeout: 15000 });
+
+        await page.getByRole('button', { name: 'Content Management' }).click();
+        await page.getByRole('link', { name: 'Interview questions bank' }).click();
+        await page.locator('tr:nth-child(7) > td:nth-child(11) > .relative > div > .p-1').click();
+        await page.getByRole('menuitem', { name: 'Update Status' }).click();
+        await page.getByLabel('Status *').selectOption('approved');
+        await page.getByRole('button', { name: 'Update Status' }).click();
     });
 
-    test("able to approve guide information/data" , async ({ page }) => {
+    test("able to approve guide information" , async ({ page }) => {
         //login with valid account first
-        await page.goto("http://adultna.com/auth/login");
-        await page.fill('input[name="email"]', "lewisdomnilo@gmail.com");
-        await page.fill('input[name="password"]', "Lewis123.");
+        await page.goto("http://adultna.com/admin/login");
+        await page.fill('input[name="email"]', "verifier@gmail.com");
+        await page.fill('input[name="password"]', "QWEasd123.");
         await page.getByRole("button", { name: "Login" }).click();
 
-        await expect(page).toHaveURL(/\/dashboard\/?$/, { timeout: 15000 });
+        await expect(page).toHaveURL(/\/admin\/dashboard\/?$/, { timeout: 15000 });
     });
 
     test("able to approve onboarding questions" , async ({ page }) => {
         //login with valid account first
-        await page.goto("http://adultna.com/auth/login");
-        await page.fill('input[name="email"]', "lewisdomnilo@gmail.com");
-        await page.fill('input[name="password"]', "Lewis123.");
+        await page.goto("http://adultna.com/admin/login");
+        await page.fill('input[name="email"]', "verifier@gmail.com");
+        await page.fill('input[name="password"]', "QWEasd123.");
         await page.getByRole("button", { name: "Login" }).click();
 
-        await expect(page).toHaveURL(/\/dashboard\/?$/, { timeout: 15000 });
+        await expect(page).toHaveURL(/\/admin\/dashboard\/?$/, { timeout: 15000 });
     });
 
-    test("able to add new questions - for both onboarding and question bank" , async ({ page }) => {
+    test("able to add new questions - for question bank" , async ({ page }) => {
         //login with valid account first
-        await page.goto("http://adultna.com/auth/login");
-        await page.fill('input[name="email"]', "lewisdomnilo@gmail.com");
-        await page.fill('input[name="password"]', "Lewis123.");
+        await page.goto("http://adultna.com/admin/login");
+        await page.fill('input[name="email"]', "verifier@gmail.com");
+        await page.fill('input[name="password"]', "QWEasd123.");
         await page.getByRole("button", { name: "Login" }).click();
 
-        await expect(page).toHaveURL(/\/dashboard\/?$/, { timeout: 15000 });
+        await expect(page).toHaveURL(/\/admin\/dashboard\/?$/, { timeout: 15000 });
+
+        await page.getByRole('button', { name: 'Content Management' }).click();
+        await page.getByRole('link', { name: 'Interview questions bank' }).click();
+
+        await page.getByRole('button', { name: 'Add Question' }).click();
+        await page.getByRole('textbox', { name: 'Question *' }).click();
+        await page.getByRole('textbox', { name: 'Question *' }).fill('How would you relate one of your hobbies towards how you conduct yourself professionally?');
+        await page.getByLabel('Category *').selectOption('behavioral');
+        await page.getByLabel('Industry *').selectOption('general');
+        await page.locator('form').getByRole('button', { name: 'Add Question' }).click();
+    });
+
+    test("able to add new questions - for onboarding questions" , async ({ page }) => {
+        //login with valid account first
+        await page.goto("http://adultna.com/admin/login");
+        await page.fill('input[name="email"]', "verifier@gmail.com");
+        await page.fill('input[name="password"]', "QWEasd123.");
+        await page.getByRole("button", { name: "Login" }).click();
+
+        await expect(page).toHaveURL(/\/admin\/dashboard\/?$/, { timeout: 15000 });
+
+        await page.getByRole('button', { name: 'Content Management' }).click();
+        await page.getByRole('link', { name: 'Onboarding questions' }).click();
+
+
+        await page.getByRole('button', { name: 'Add Question' }).click();
+        await page.getByRole('textbox', { name: 'Question *' }).click();
+        await page.getByRole('textbox', { name: 'Question *' }).fill('Which age group do you define yourself belonging to?');
+        await page.getByRole('textbox', { name: 'Option Text *' }).click();
+        await page.getByRole('textbox', { name: 'Option Text *' }).fill('Young adult');
+        await page.getByRole('button', { name: '+ Add Option' }).click();
+        await page.locator('#option-text-1').click();
+        await page.locator('#option-text-1').fill('Seasoned Adult');
+        await page.getByRole('button', { name: '+ Add Option' }).click();
+        await page.locator('#option-text-2').click();
+        await page.locator('#option-text-2').fill('Geriatric Milennial');
+        await page.getByRole('button', { name: 'Create Question' }).click();
     });
 
     test("able to add new guides" , async ({ page }) => {
         //login with valid account first
-        await page.goto("http://adultna.com/auth/login");
-        await page.fill('input[name="email"]', "lewisdomnilo@gmail.com");
-        await page.fill('input[name="password"]', "Lewis123.");
+        await page.goto("http://adultna.com/admin/login");
+        await page.fill('input[name="email"]', "verifier@gmail.com");
+        await page.fill('input[name="password"]', "QWEasd123.");
         await page.getByRole("button", { name: "Login" }).click();
 
-        await expect(page).toHaveURL(/\/dashboard\/?$/, { timeout: 15000 });
+        await expect(page).toHaveURL(/\/admin\/dashboard\/?$/, { timeout: 15000 });
+
+
+        await page.getByRole('button', { name: 'Content Management' }).click();
+        await page.getByRole('link', { name: 'Government processes' }).click();
+        await page.getByRole('button', { name: 'Add Guide' }).click();
+        await page.getByRole('textbox', { name: 'Guide Title *' }).fill('How to Apply for a Business Permit');
+        await page.getByRole('textbox', { name: 'Issuing Agency *' }).fill('BPLO');
+        await page.getByLabel('Category *').selectOption('permits-licenses');
+        await page.getByRole('textbox', { name: 'Processing Time' }).fill('1 week');
+        await page.getByRole('textbox', { name: 'Step 1 title' }).fill('Apply in your Local Government Unit');
+        await page.getByRole('textbox', { name: 'Requirement name (e.g., Valid' }).fill('Valid ID');
+        await page.getByRole('button', { name: 'Create Guide' }).click();
     });
 });
